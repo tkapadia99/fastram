946ae7d0c1e7e814708c992dbe28a65c02d4d0b1
Tyler <tyler@fastram.com>
2025-02-17 19:20:05 -0600

    feat: add initial files and create homepage with navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <main>
+        <h1>Welcome to My About Page</h1>
+      </main>
+    );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+      <main>
+        <h1>Welcome to My Contact Page</h1>
+      </main>
+    );
+  }
